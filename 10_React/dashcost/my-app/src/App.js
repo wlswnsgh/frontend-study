272777a62c95653a5f4ea086dbc69c63fb75a2fe
jinjnnho <junho11_kr@naver.com>
2024-05-08 13:46:22 +0900

    2024.05.08

--- 10_React/dashcost/my-app/src/App.js
+++ 10_React/dashcost/my-app/src/App.js
@@ -8,10 +8,10 @@ function App() {
   const [posts, setposts] = useState(['리액트 잘 쓰려면?', '자바스크립트 핵심 문법', 'css 스타일링 가이트']);
 
   const [showPostDetail, setshowPostDetail] = useState(false);
-
-  const handleClick = () => {
-    setshowPostDetail(!showPostDetail);
-  };
+  const [currentIndex, setCurrentIndex] = useState(null);
+  // const handleClick = () => {
+  //   setshowPostDetail(!showPostDetail);
+  // };
 
 
   return (
@@ -49,19 +49,30 @@ function App() {
         {/* Quiz: map함수를 이용해서 posts 반복 랜더링을 해주어라 */}
         
         {/* 방법1 */}
-        {posts.map((num, index) => (
-          <div key={index} className='list'>
+        {posts.map((num, index) => {
+          return (
+          <div key={index} className='list'
+            onClick={() => {
+              setshowPostDetail(true);
+              setCurrentIndex(index); // id값이 없으니 대신...
+            }}
+          >
+
             <h4>{num}</h4>
             <p>2023년 1월 20일</p>
             <p>by junho.jin</p>
           </div>
-        ))}
+          );
+        
+        })}
+
+        {/* <p>{showPostDetail ? <PostDetail /> : null }</p>
+        <button type='button' onClick={handleClick}>누르면 나와요^^</button> */}
 
         {/* 포스트 상세보기 */}
         {/* Quiz: 조건부 랜더링 */}
-        <p>{showPostDetail ? <PostDetail />: null }</p>
-
-        <button type='button' onClick={handleClick}>누르면 나와요^^</button>
+        {showPostDetail &&  <PostDetail posts = {posts} currentIndex={currentIndex}/>}
+        
       </div>
     </>
   );
