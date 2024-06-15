abcdf9e7d9fbea89d1c873768df6f506f4789a14
jin <junho11_kr@naver.com>
2024-06-15 21:09:17 +0900

    24.06.15

--- 10_React/my-app/src/chapter16/pages/AboutPage.jsx
+++ 10_React/my-app/src/chapter16/pages/AboutPage.jsx
@@ -9,7 +9,7 @@ function AboutPage() {
   // 첫번째 요소는 쿼리스트링을 조회하거나 수정하는  메서드가 담긴 객체를 반환
   const [searchParams, setSearchParams] = useSearchParams();
   const name = searchParams.get('name');
-  const age = searchParams.get('age');
+  const age = setSearchParams.get('age');
 
   return (
     <>
